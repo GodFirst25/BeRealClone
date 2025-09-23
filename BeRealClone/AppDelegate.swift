07b811aa602f1c1a4c48161e956e2c4d1ad3e6ae
student <student@hubmac12.lib.fiu.edu>
2025-09-22 23:13:33 -0400

    Initial commit

---
+++ BeRealClone/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  BeRealClone
+//
+//  Created by student on 9/22/25.
+//
+
+import UIKit
+import ParseSwift
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+}
---
+++ BeRealClone/BeRealCloneApp.swift
@@ -0,0 +1,19 @@
+//
+//  BeRealCloneApp.swift
+//  BeRealClone
+//
+//  Created by student on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
